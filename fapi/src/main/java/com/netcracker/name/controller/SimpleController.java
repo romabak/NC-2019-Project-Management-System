0b9201c6302964786b89f2cc3efe7ddfdaa87e7c
romabak <bakanouskiroman@gmail.com>
2019-03-18 00:57:21 +0300

    frontend commit

--- fapi/src/main/java/com/netcracker/name/controller/SimpleController.java
+++ fapi/src/main/java/com/netcracker/name/controller/SimpleController.java
@@ -15,5 +15,25 @@ public class SimpleController {
 
     class SimpleObject {
 
+        String id;
+        String name;
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private  http: HttpClient){
+
+  }
+
+  public clickFunction(): void {
+    this.http.get("/api").subscribe(value => {
+      console.log(value);
+    })
+  }
 }
